551bec31b9b70df896879ed079ffc1036966b694
Pascal Haakmat <pascal@high-juice.com>
2021-09-07 08:56:49 +0200

    Improve error handling

--- funda.service/FundaService.cs
+++ funda.service/FundaService.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using funda.Services;
 using System.Collections.Generic;
-using System.Linq;
 using RateLimiter;
 using ComposableAsync;
+using System.ServiceModel.Security;
 
-namespace funda
+namespace funda.service
 {
-    public class FundaServiceException : Exception {
-        public FundaServiceException(Exception ex) {
-        }
-    }
     public interface IFundaService {
         IAanbod GetClient();
         Task<IEnumerable<Object1>> GetAllObjects(string zo);
@@ -36,26 +30,57 @@ namespace funda
             return _client;
         }
 
+        /// <summary>
+        /// Fetches all objects from the funda API that match the query given by <paramref name="zo"/>.
+        /// </summary>
+        /// <param name="zo">A search path like "/amsterdam"</param>
+        /// <returns>List of objects</returns>
         public async Task<IEnumerable<Object1>> GetAllObjects(string zo) {
             var all = new List<Object1>();
-            var totalPages = 0;
-            var pageIndex = 0;
-            var pageSize = 25;
+            var totalPages = -1;
+            var pageIndex = 1;
+            var pageSize = 20;
+            var totalObjects = -1;
+            var retryMax = 4;
+            var retryAttempt = 0;
+            var retryDelay = TimeSpan.FromSeconds(15);
             var client = GetClient();
             do
             {
                 await _rateLimiter;
-                var response = await client.ZoekAanbodAsync(new ZoekAanbodRequest
+                try
                 {
-                    key = _options.ApiKey,
-                    aanbodType = "koop",
-                    zoekPad = zo,
-                    page = $"{pageIndex}",
-                    pagesize = $"{pageSize}"
-                });
-                totalPages = response.ZoekAanbodResult.TotaalAantalObjecten / pageSize;
-                all.AddRange(response.ZoekAanbodResult.Objects);
-            } while (pageIndex++ < totalPages);
+                    _logger.LogDebug($"loading page {pageIndex}...");
+                    var response = await client.ZoekAanbodAsync(new ZoekAanbodRequest
+                    {
+                        key = _options.ApiKey,
+                        aanbodType = "koop",
+                        zoekPad = zo,
+                        page = $"{pageIndex}",
+                        pagesize = $"{pageSize}"
+                    });
+                    if (totalObjects != -1 && totalObjects != response.ZoekAanbodResult.TotaalAantalObjecten)
+                        _logger.LogWarning($"number of objects changed while fetching, previous={totalObjects}, current={response.ZoekAanbodResult.TotaalAantalObjecten}");
+                    totalObjects = response.ZoekAanbodResult.TotaalAantalObjecten;
+                    totalPages = (int)Math.Ceiling((double)totalObjects / pageSize);
+                    _logger.LogDebug($"loaded page {pageIndex}, totalPages={totalPages}, # objects={response.ZoekAanbodResult.Objects.Length}");
+                    all.AddRange(response.ZoekAanbodResult.Objects);
+                    pageIndex++;
+                    retryAttempt = 0;
+                } 
+                catch (MessageSecurityException ex)
+                {
+                    if (retryAttempt++ < retryMax) {
+                        _logger.LogWarning($"Error {ex.Message}, retrying after {retryDelay}...");
+                        await Task.Delay(retryDelay);
+                    } else {
+                        _logger.LogError(ex, $"Error loading page {pageIndex}, retried {retryAttempt-1} times");
+                        throw;
+                    }
+                }
+            } while (totalPages == -1 || pageIndex <= totalPages);
+            if (all.Count != totalObjects)
+                _logger.LogWarning($"Got different total than expected: all.Count={all.Count}, totalObjects={totalObjects}");
             return all;
         }
     }
